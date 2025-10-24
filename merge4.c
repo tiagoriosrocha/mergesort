f1484dac9bbb49fd4ae3fed1c9019284d40ee255
Tiago Rocha <tiagoriosrocha@gmail.com>
2025-10-24 17:06:49 -0300

    apresentacao

--- merge4.c
+++ merge4.c
@@ -6,11 +6,11 @@
 #define NUM_THRESHOLDS 8
 
 // Thresholds a testar
-int thresholds[NUM_THRESHOLDS] = { -1, 1000, 100, 50, 25, 15, 10, 5 };  // -1 significa "merge sort puro"
+int thresholds[NUM_THRESHOLDS] = {-1, 1000, 100, 50, 25, 15, 10, 5}; // -1 significa "merge sort puro"
 
 // Tamanhos dos vetores
 #define NUM_SIZES 3
-int sizes[NUM_SIZES] = { 100000, 10000000, 100000000 };
+int sizes[NUM_SIZES] = {100000, 10000000, 100000000};
 
 // Prototipagem
 void merge(int *array, int *temp, int left, int mid, int right);
@@ -19,10 +19,14 @@ void insertionSort(int *array, int left, int right);
 void mergeSort(int *array, int *temp, int left, int right);
 
 // Função Bubble Sort
-void bubbleSort(int array[], int left, int right) {
-    for (int i = left; i <= right; i++) {
-        for (int j = left; j < right - (i - left); j++) {
-            if (array[j] > array[j + 1]) {
+void bubbleSort(int array[], int left, int right)
+{
+    for (int i = left; i <= right; i++)
+    {
+        for (int j = left; j < right - (i - left); j++)
+        {
+            if (array[j] > array[j + 1])
+            {
                 int tmp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = tmp;
@@ -32,22 +36,31 @@ void bubbleSort(int array[], int left, int right) {
 }
 
 // Merge com vetor temporário global
-void merge(int *array, int *temp, int left, int mid, int right) {
+void merge(int *array, int *temp, int left, int mid, int right)
+{
     int i = left, j = mid + 1, k = left;
-    for (int l = left; l <= right; l++) temp[l] = array[l];
+    for (int l = left; l <= right; l++)
+        temp[l] = array[l];
 
-    while (i <= mid && j <= right) {
-        if (temp[i] <= temp[j]) array[k++] = temp[i++];
-        else array[k++] = temp[j++];
+    while (i <= mid && j <= right)
+    {
+        if (temp[i] <= temp[j])
+            array[k++] = temp[i++];
+        else
+            array[k++] = temp[j++];
     }
 
-    while (i <= mid) array[k++] = temp[i++];
-    while (j <= right) array[k++] = temp[j++];
+    while (i <= mid)
+        array[k++] = temp[i++];
+    while (j <= right)
+        array[k++] = temp[j++];
 }
 
 // Merge Sort puro
-void mergeSort(int *array, int *temp, int left, int right) {
-    if (left < right) {
+void mergeSort(int *array, int *temp, int left, int right)
+{
+    if (left < right)
+    {
         int mid = left + (right - left) / 2;
         mergeSort(array, temp, left, mid);
         mergeSort(array, temp, mid + 1, right);
@@ -56,10 +69,14 @@ void mergeSort(int *array, int *temp, int left, int right) {
 }
 
 // Merge Sort híbrido com insertion para sub-vetores pequenos
-void hybridSort(int *array, int *temp, int left, int right, int threshold) {
-    if (right - left + 1 <= threshold) {
+void hybridSort(int *array, int *temp, int left, int right, int threshold)
+{
+    if (right - left + 1 <= threshold)
+    {
         bubbleSort(array, left, right);
-    } else {
+    }
+    else
+    {
         int mid = left + (right - left) / 2;
         hybridSort(array, temp, left, mid, threshold);
         hybridSort(array, temp, mid + 1, right, threshold);
@@ -68,10 +85,12 @@ void hybridSort(int *array, int *temp, int left, int right, int threshold) {
 }
 
 // Função para testar uma única estratégia
-double test_sort(int *original, int n, int threshold) {
+double test_sort(int *original, int n, int threshold)
+{
     int *array = malloc(n * sizeof(int));
-    int *temp  = malloc(n * sizeof(int));
-    if (!array || !temp) {
+    int *temp = malloc(n * sizeof(int));
+    if (!array || !temp)
+    {
         printf("Erro ao alocar memória\n");
         exit(1);
     }
@@ -81,9 +100,9 @@ double test_sort(int *original, int n, int threshold) {
     clock_t start = clock();
 
     if (threshold == -1)
-        mergeSort(array, temp, 0, n - 1);  // Merge puro
+        mergeSort(array, temp, 0, n - 1); // Merge puro
     else
-        hybridSort(array, temp, 0, n - 1, threshold);  // Híbrido
+        hybridSort(array, temp, 0, n - 1, threshold); // Híbrido
 
     clock_t end = clock();
     double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
@@ -94,11 +113,13 @@ double test_sort(int *original, int n, int threshold) {
     return time_spent;
 }
 
-int main() {
-    srand(42);  // Semente fixa
+int main()
+{
+    srand(42); // Semente fixa
 
     printf("Tamanho\\Threshold");
-    for (int i = 0; i < NUM_THRESHOLDS; i++) {
+    for (int i = 0; i < NUM_THRESHOLDS; i++)
+    {
         if (thresholds[i] == -1)
             printf("\tMerge");
         else
@@ -106,22 +127,26 @@ int main() {
     }
     printf("\n");
 
-    for (int s = 0; s < NUM_SIZES; s++) {
+    for (int s = 0; s < NUM_SIZES; s++)
+    {
         int n = sizes[s];
         printf("%d", n);
 
         // Gera vetor original uma vez
         int *original = malloc(n * sizeof(int));
-        if (!original) {
+        if (!original)
+        {
             printf("Erro ao alocar vetor original\n");
             return 1;
         }
 
-        for (int i = 0; i < n; i++) {
-            original[i] = rand();  // Sempre a mesma sequência
+        for (int i = 0; i < n; i++)
+        {
+            original[i] = rand(); // Sempre a mesma sequência
         }
 
-        for (int t = 0; t < NUM_THRESHOLDS; t++) {
+        for (int t = 0; t < NUM_THRESHOLDS; t++)
+        {
             double time = test_sort(original, n, thresholds[t]);
             printf("\t%.2f", time);
         }
@@ -133,6 +158,4 @@ int main() {
     return 0;
 }
 
-
-
-//gcc -O2 -o hybrid_benchmark hybrid_benchmark.c
+// gcc -O2 -o hybrid_benchmark hybrid_benchmark.c
